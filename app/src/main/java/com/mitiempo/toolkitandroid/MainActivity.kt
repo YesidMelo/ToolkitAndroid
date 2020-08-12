d9bfa259ddc77d98f246992f1d02e6d903a9aae7
Yesid <trigoperfec3@hotmail.com>
2020-08-12 14:37:10 -0500

    implementacion de proxyretrofitrx en proyecto principal de pruebas funcionando

--- app/src/main/java/com/mitiempo/toolkitandroid/MainActivity.kt
+++ app/src/main/java/com/mitiempo/toolkitandroid/MainActivity.kt
@@ -2,11 +2,30 @@ package com.mitiempo.toolkitandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.mitiempo.toolkitandroidclases.DataAccess.retrofit.ProxyRetrofitRx
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hola_mundo.setOnClickListener {
+            ProxyRetrofitRx()
+                .conUrlBase("http://192.168.0.3:3000/")
+                .conEscuchadorCodigoRespuesta {
+                    Log.e("codigoRespuesta","codigo : ${it}")
+                }
+                .conEscuchadorFalla {
+                    Log.e("Error","Fallo detalle : ",it)
+                }
+                .conEscuchadorRespuestaExitosa {
+                    Log.e("Respuesta","buena .... ")
+                }
+                .conServicio(Servicios.traerGet)
+                .realizarConsulta()
+        }
     }
 }
---
+++ app/src/main/java/com/mitiempo/toolkitandroid/Servicios.kt
@@ -0,0 +1,46 @@
+package com.mitiempo.toolkitandroid
+
+import com.mitiempo.toolkitandroidclases.DataAccess.retrofit.IRetrofitParcelable
+import com.mitiempo.toolkitandroidclases.DataAccess.retrofit.IServiceParameters
+
+enum class Servicios(
+    private val url: String,
+    private val metodo : IServiceParameters.Methods
+) : IServiceParameters {
+    traerGet("",IServiceParameters.Methods.GET)
+    ;
+
+    override fun getURL(): String {
+        return url + complemento
+    }
+
+    override fun getMethods(): IServiceParameters.Methods {
+        return metodo
+    }
+
+    private var complemento = ""
+    override fun WithComplement(complement: String): IServiceParameters {
+        this.complemento = complement
+        return this
+    }
+
+    private var objetoAEnviar : IRetrofitParcelable ?= null
+    override fun <T : IRetrofitParcelable> conObjetoAEnviar(objetoAEnviar: T): IServiceParameters {
+        this.objetoAEnviar = objetoAEnviar
+        return this
+    }
+
+    override fun traerObjetoAEnviar(): IRetrofitParcelable? {
+        return objetoAEnviar
+    }
+
+    private var claseObjetoEsperado : Class<*> ?= null
+    override fun <T : IRetrofitParcelable> conObjetoEsperado(objetoEsperado: Class<T>): IServiceParameters {
+        this.claseObjetoEsperado = objetoEsperado
+        return this
+    }
+
+    override fun traerClaseObjetoEsperado(): Class<*>? {
+        return claseObjetoEsperado
+    }
+}
